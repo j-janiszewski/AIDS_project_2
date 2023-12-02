641473614505f1584b599929ccb74902e704a959
jan <jasiekj1@gmail.com>
2023-12-02 13:06:39 +0000

    assignment_2

--- assignment_2/task_1.py
+++ assignment_2/task_1.py
@@ -1,3 +1,10 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, DataType, StringType, IntegerType
 
+spark = SparkSession.builder.master("local[3]").appName("SparkSchemaDemo").getOrCreate()
+
+
+temperature_df = spark.read.format("csv").options(inferSchema=True).load("data/1800.csv")
+temperature_df = temperature_df.selectExpr("_c0 as name", "_c2 as obs_type", "_c3 as temp")
+
+temperature_df=temperature_df.filter("(name == 'ITE00100554' or name == 'EZE00100082') and obs_type == 'TMIN' ")
+temperature_df.groupby("name").min("temp").show()
---
+++ assignment_2/task_4.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master("local[3]").appName("SparkSchemaDemo").getOrCreate()
+
+
+customer_df = spark.read.format("csv").options(inferSchema=True).load("data/customer-orders.csv")
+customer_df = customer_df.selectExpr("_c0 as id", "_c1 as prod_id", "_c2 as amount")
+
+
+customer_df.groupby("id").sum("amount").show()
