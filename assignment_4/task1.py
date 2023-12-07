844ae5b86890ee6213c2d7195a450a9eab8bf1b3
jan <jasiekj1@gmail.com>
2023-12-07 21:39:32 +0000

    assignment_4

--- assignment_4/task1.py
+++ assignment_4/task1.py
@@ -1,5 +1,32 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
 
 spark = SparkSession.builder.master("local[3]").appName("SparkSchemaDemo").getOrCreate()
 
-access_lines = spark.readStream
+lines = spark \
+    .readStream \
+    .format("socket") \
+    .option("host", "0.0.0.0") \
+    .option("port", 9995) \
+    .load()
+
+# Split the lines into words
+words = lines.select(
+   explode(
+       split(lines.value, " ")
+   ).alias("word")
+)
+
+# Generate running word count
+wordCounts = words.groupBy("word").count()
+
+
+query = wordCounts \
+    .writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .start()
+query.awaitTermination()
+
+spark.stop()
