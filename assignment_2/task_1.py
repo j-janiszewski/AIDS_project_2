0f6cedbcdcf92378488bedfdeafb12d768199119
jan <jasiekj1@gmail.com>
2023-12-12 23:59:07 +0000

    new file names

--- assignment_2/task_1.py
+++ assignment_2/task_1.py
@@ -2,14 +2,16 @@ from pyspark import SparkConf, SparkContext
 import os
 
 conf = SparkConf().setMaster("local").setAppName("MinTemp")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
-lines = sc.textFile('1800.csv')
+lines = sc.textFile("../data/1800.csv")
+
 
 def parse_line(line):
     (city, _, _, temp, _, _, _, _) = line.split(",")
-    temperature = int(temp)/10
+    temperature = int(temp) / 10
     return (city, temperature)
+
 
 rdd = lines.filter(lambda x: x.split(",")[2] == "TMIN").map(parse_line)
 
--- assignment_2/task_4_and_5.py
+++ assignment_2/task_4_and_5.py
@@ -2,18 +2,20 @@ from pyspark import SparkConf, SparkContext
 import os
 
 conf = SparkConf().setMaster("local").setAppName("WordFrequency")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
 
-lines = sc.textFile('customer-orders.csv')
+lines = sc.textFile("../data/customer-orders.csv")
+
 
 def parse_line(line):
     customer_id, _, amount = line.split(",")
     return (customer_id, float(amount))
 
+
 rdd = lines.map(parse_line)
 
-spendings = rdd.reduceByKey(lambda x,y: x + y).sortBy(lambda x: x[1], ascending=False)
+spendings = rdd.reduceByKey(lambda x, y: x + y).sortBy(lambda x: x[1], ascending=False)
 
 for i in spendings.collect():
     print(i)
